156ad99c6405cbdb24bbfa1eed6dde62515a7d3a
Indra Rizky Gunawan <60083946+indrarz@users.noreply.github.com>
2020-05-08 13:31:29 +0700

    Update prosesregis.php

--- prosesregis.php
+++ prosesregis.php
@@ -7,30 +7,32 @@ if(isset($_POST['Daftar'])){
 	$email = $_POST['Email'];
 	$username = $_POST['Username'];
 	$password = $_POST['Password'];
+	$password = md5($password);
 	$namadepan = $_POST['FName'];
 	$namabelakang = $_POST['LName'];
 
 	// Cek agar tidak ada username atau email yang sama
 	$cek_username = pg_num_rows(pg_query("SELECT * FROM Pengguna WHERE Username = '$username'"));
 	$cek_email = pg_num_rows(pg_query("SELECT * FROM Pengguna WHERE Email = '$email'"));
+
 	if($cek_username > 0 || $cek_email > 0) {
-		echo "<script>alert('Username atau Email sudah pernah digunakan'); history.go(-1) </script>";
+		echo "<script>alert('Username atau Email sudah terdaftar'); history.go(-1) </script>";
 	} else {
 		// buat query
 		$query = pg_query("INSERT INTO Pengguna (Email, Username, Password, FName, LName) VALUES ('$email', '$username', '$password', '$namadepan', '$namabelakang')");
 
 		// apakah query simpan berhasil?
 		if( $query==TRUE ) {
-			// kalau berhasil alihkan ke halaman index.php dengan status=sukses
-			header('Location: index.php?status=sukses');
+			// kalau berhasil alihkan ke halaman formlogin.php dengan status=sukses
+			header('Location: formlogin.php?info=sukses');
 		} else {
-			// kalau gagal alihkan ke halaman indek.php dengan status=gagal
-			header('Location: index.php?status=gagal');
+			// kalau gagal alihkan ke halaman formregis.php dengan status=gagal
+			header('Location: formregis.php?info=gagal');
 		}
 	}
 
 } else {
-	die("Akses dilarang...");
+	die("Terjadi masalah...");
 }
 
 ?>
