0076b0b8b25e0d2ee740e6e928d149e0d37929a1
Indra Rizky Gunawan <60083946+indrarz@users.noreply.github.com>
2020-05-10 23:51:01 +0700

    Index

--- index.php
+++ index.php
@@ -1,61 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
 
-    <!-- My Fonts -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Viga">
+  <!-- My Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Viga">
 
-    <!-- My Icon -->
-    <link rel="shortcut icon" href="img/favicon.png">
+  <!-- My Icon -->
+  <link rel="shortcut icon" href="img/favicon.png">
 
-    <!-- My CSS -->
-    <link rel="stylesheet" href="style.css">
+  <!-- My CSS -->
+  <link rel="stylesheet" href="style.css">
 
-    <title>smallFarm</title>
+  <title>smallFarm</title>
 </head>
 
 <body class="body">
-	<!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light">
-      <a class="navbar-brand" href="index.php">small<span>Farm</span></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-          <div class="navbar-nav ml-auto">
-            <a class="nav-item btn btn-primary login" href="formlogin.php">Login</a>
-            <a class="nav-item btn btn-primary daftar" href="formregis.php">Daftar</a>
-          </div>
-      </div>
-    </nav>    
-    <!-- Akhir Navbar -->
-
-    <!-- Jumbotron -->
-    <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1 class="display-4"><span>smallFarm</span> merupakan sebuah website yang dibuat dengan <span>tujuan</span> membantu masyarakat yang ingin belajar mengenai <span>pertanian</span> dalam skala kecil.</h1>
-      </div>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="index.php">small<span>Farm</span></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="navbar-nav ml-auto">
+          <a class="nav-item btn btn-primary login" href="formlogin.php">Login</a>
+          <a class="nav-item btn btn-primary daftar" href="formregis.php">Daftar</a>
+        </div>
     </div>
-    <!-- Akhir Jumbotron -->
+  </nav>    
+  <!-- Akhir Navbar -->
 
-    <!-- Footer -->
-    <footer class="page-footer">
-      <div class="footer">© smallFarm, 2020</div>
-    </footer>
-    <!-- Akhir footer -->
+  <!-- Jumbotron -->
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4"><span>smallFarm</span> merupakan sebuah website yang dibuat dengan <span>tujuan</span> membantu masyarakat yang ingin belajar mengenai <span>pertanian</span> dalam skala kecil.</h1>
+    </div>
+  </div>
+  <!-- Akhir Jumbotron -->
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="js/bootstrap.min.js"></script>
+  <!-- Footer -->
+  <footer class="page-footer">
+    <div class="footer">© smallFarm, 2020</div>
+  </footer>
+  <!-- Akhir footer -->
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="js/bootstrap.min.js"></script>
 
 </body>
 </html>
