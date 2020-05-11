87a4709c9d8c14236cc14b2407662602fef357b6
Indra Rizky Gunawan <60083946+indrarz@users.noreply.github.com>
2020-05-12 01:22:05 +0700

    Form Regis

--- formregis.php
+++ formregis.php
@@ -11,12 +11,13 @@
 
   <!-- My Fonts -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Viga">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
 
   <!-- My Icon -->
   <link rel="shortcut icon" href="img/favicon.png">
 
   <!-- My CSS -->
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="style-1.css">
 
   <title>Registrasi | smallFarm</title>
 </head>
@@ -31,46 +32,46 @@
   <!-- Jumbotron -->
   <div class="jumbotron jumbotron-fluid">
     <div class="container">
-    	<!-- Form -->
-	<form class="form" action="prosesregis.php" method="POST">
-	  <fieldset>
-	    <div class="container">
-	      <p class="regis">Registrasi</p>
-	      <?php if (isset($_GET['info'])) { ?>
-		<?php if ($_GET['info'] == 'tersedia') { ?>
-		  <div class="alert alert-danger" role="alert">Username atau Email telah terdaftar.</div>
-      		<?php } ?>
+      <!-- Form -->
+      <form class="form" action="prosesregis.php" method="POST">
+        <fieldset>
+          <div class="container">
+            <p class="regis">Registrasi</p>
+            <?php if (isset($_GET['info'])) { ?>
+              <?php if ($_GET['info'] == 'tersedia') { ?>
+                <div class="alert alert-danger" role="alert">Username atau Email telah terdaftar.</div>
               <?php } ?>
-	      <p>
-		<div class="row">
-		  <div class="col">
-		    <input type="text" class="form-control" name="FName" placeholder="Nama depan" required oninvalid="this.setCustomValidity('Data tidak boleh kosong')" oninput="setCustomValidity('')">
-		  </div>
-		  <div class="col">
-		    <input type="text" class="form-control" name="LName" placeholder="Nama belakang" required oninvalid="this.setCustomValidity('Data tidak boleh kosong')" oninput="setCustomValidity('')">
-		  </div>
-		</div>
-	      </p>
-	      <p>
-		<input type="email" class="form-control" name="Email" placeholder="Email" required oninvalid="this.setCustomValidity('Data tidak boleh kosong')" oninput="setCustomValidity('')">
-	      </p>
-	      <p>
-		<input type="text" class="form-control" name="Username" placeholder="Username" required oninvalid="this.setCustomValidity('Data tidak boleh kosong')" oninput="setCustomValidity('')">
-	      </p>
-	      <p>
-		<input type="password" class="form-control" name="Password" placeholder="Password" required oninvalid="this.setCustomValidity('Data tidak boleh kosong')" oninput="setCustomValidity('')">
-	      </p>
-	      <p class="tombol">
-		<button type="submit" class="btn btn-primary sub-daftar" name="Daftar">Daftar</button> 
-	      </p>
-	    </div>
-	  </fieldset>
-	</form>
-    	<!-- Akhir Form -->
+            <?php } ?>
+            <p>
+              <div class="row">
+                <div class="col">
+                  <input type="text" class="form-control" name="FName" placeholder="Nama depan" required oninvalid="this.setCustomValidity('Data tidak boleh kosong')" oninput="setCustomValidity('')">
+                </div>
+                <div class="col">
+                  <input type="text" class="form-control" name="LName" placeholder="Nama belakang" required oninvalid="this.setCustomValidity('Data tidak boleh kosong')" oninput="setCustomValidity('')">
+                </div>
+              </div>
+            </p>
+            <p>
+              <input type="email" class="form-control" name="Email" placeholder="Email" required oninvalid="this.setCustomValidity('Data tidak boleh kosong')" oninput="setCustomValidity('')">
+            </p>
+            <p>
+              <input type="text" class="form-control" name="Username" placeholder="Username" required oninvalid="this.setCustomValidity('Data tidak boleh kosong')" oninput="setCustomValidity('')">
+            </p>
+            <p>
+              <input type="password" class="form-control" name="Password" placeholder="Password" required oninvalid="this.setCustomValidity('Data tidak boleh kosong')" oninput="setCustomValidity('')">
+            </p>
+            <p class="tombol">
+              <button type="submit" class="btn btn-primary sub-daftar" name="Daftar">Daftar</button> 
+            </p>
+          </div>
+        </fieldset>
+      </form>
+      <!-- Akhir Form -->
 
-    	<p class="back">
-	  <a class="back-login" href="formlogin.php">Sudah punya akun? Masuk!</a>
-	</p>
+      <p class="back">
+        <a class="back-login" href="formlogin.php">Sudah punya akun? Masuk!</a>
+      </p>
     </div>
   </div>
   <!-- Akhir Jumbotron -->
