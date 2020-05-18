1cda341f2569cfeb76cebad7dadd78feafb77fec
anggario21 <60083608+anggario21@users.noreply.github.com>
2020-05-18 08:14:11 +0700

    Update penjualan.php

--- penjualan.php
+++ penjualan.php
@@ -28,7 +28,7 @@ if (!isset($_SESSION['Username'])) {
   <!-- My CSS -->
   <link rel="stylesheet" href="style-2.css">
 
-  <title>Dashboard | smallFarm</title>
+  <title>Penjualan | smallFarm</title>
 </head>
 
 <body class="body">
