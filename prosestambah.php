fcca6a64f28e9c56b28f955fe7e6ded8e453ec96
anggario21 <60083608+anggario21@users.noreply.github.com>
2020-06-21 13:57:03 +0700

    Add files via upload

---
+++ prosestambah.php
@@ -0,0 +1,31 @@
+
+<?php
+include("config.php");
+// cek apakah tombol Tambah Barang sudah diklik atau belum?
+if(isset($_POST['Tambah'])){
+
+	// ambil data dari formulir
+	$Id = $_POST['Id_produk'];
+	$Nama = $_POST['Nama_produk'];
+	$Stok = $_POST['Stok_produk'];
+	$Harga = $_POST['Harga_produk'];
+
+
+	// buat query
+	$query = pg_query("INSERT INTO produk (Id_produk ,Nama_produk, Stok_produk, Harga_produk) VALUES ('$Id','$Nama', '$Stok', '$Harga')");
+
+	// apakah query berhasil?
+		if($query==TRUE) {
+			// kalau berhasil alihkan ke halaman tambahbarang.php dengan status=sukses
+			header('Location: tambahbarang.php?info=sukses');
+		} else {
+			// kalau gagal alihkan ke halaman tambahbarang.php dengan status=gagal
+			header('Location: tambahbarang.php?info=gagal');
+			}
+		}
+
+	else {
+	die("Terjadi masalah...");
+}
+
+?>
